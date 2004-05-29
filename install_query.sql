678d6c1b6350255caf58598d6589f435453d5ff4
christopherfink <christopherfink>
2004-05-29 16:49:36 +0000

    Blank ReadMe, table structure in install_query

---
+++ install_query.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `anyInventory_categories` (
+  `id` int(11) NOT NULL auto_increment,
+  `parent` int(11) NOT NULL default '0',
+  `name` varchar(32) NOT NULL default '',
+  UNIQUE KEY `id` (`id`),
+  KEY `parent` (`parent`)
+) TYPE=MyISAM;
+
+CREATE TABLE `anyInventory_fields` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(32) NOT NULL default '',
+  `input_type` enum('text','textarea','checkbox','radio','select','multiple') NOT NULL default 'text',
+  `values` text NOT NULL,
+  `default_value` varchar(32) NOT NULL default '',
+  `size` int(11) NOT NULL default '0',
+  `categories` text NOT NULL,
+  `importance` int(11) NOT NULL default '0',
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`name`)
+) TYPE=MyISAM;
+
+CREATE TABLE `anyInventory_items` (
+  `id` int(11) NOT NULL auto_increment,
+  `item_category` int(11) NOT NULL default '0',
+  UNIQUE KEY `id` (`id`)
+) TYPE=MyISAM;

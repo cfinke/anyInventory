6eb60cba81bae0e9ce5b702eac2592b57d571c46
christopherfink <christopherfink>
2004-06-03 21:40:50 +0000

    Added the ability to limit the search to a category.

--- search.php
+++ search.php
@@ -4,12 +4,27 @@ include("globals.php");
 
 $title = "anyInventory: Search";
 
+if (!isset($_REQUEST["c"])) $_REQUEST["c"] = 0;
+
 if (!$_REQUEST["action"]){
 	$output .= '
 		<form action="'.$_SERVER["PHP_SELF"].'" method="get">
+			<table>
+				<tr>
+					<td class="form_label"><label for="c">Limit search to:</label></td>
+					<td class="form_input">
+						<select name="c" id="c">
+							'.get_category_options($_REQUEST["c"]).'
+						</select> <input type="submit" name="submit" value="Submit" />
+					</td>
+				</tr>
+			</table>
+		</form>
+		<form action="'.$_SERVER["PHP_SELF"].'" method="get">
 			<h2>Search</h2>
-						<input type="hidden" name="action" value="do_edit" />
+				<input type="hidden" name="action" value="do_edit" />
 				<input type="hidden" name="id" value="'.$_REQUEST["id"].'" />
+				<input type="hidden" name="c" value="'.$_REQUEST["c"].'" />
 				<table>
 					<tr>
 						<td class="form_label"><label for="name">Name:</label></td>
@@ -22,46 +37,48 @@ if (!$_REQUEST["action"]){
 	while($row = mysql_fetch_array($result)){
 		$field = new field($row["id"]);
 		
-		$output .= '
-			<tr>
-				<td class="form_label"><label for="'.str_replace(" ","_",$field->name).'">'.$field->name.':</label></td>
-				<td class="form_input">';
-		
-		switch($field->input_type){
-			case 'multiple':
-				$output .= '<input type="text" id="'.str_replace(" ","_",$field->name).'_text" name="'.str_replace(" ","_",$field->name).'_text" maxlength="'.$field->size.'" value="" />';
-				$output .= '<select name="'.str_replace(" ","_",$field->name).'_select" id="'.str_replace(" ","_",$field->name).'_select">';
-				$output .= '<option value="">Select one</option>';
-				foreach($field->values as $value){
-					$output .= '<option value="'.$value.'" onclick="document.getElementById(\''.str_replace(" ","_",$field->name).'_text\').value = \''.$value.'\';">'.$value.'</option>';
-				}
-				$output .= '<input type="text" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'" maxlength="'.$field->size.'" value="" />';
-				break;
-			case 'select':
-				$output .= '<select name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'">
-					<option value="">Select one</option>';
-				foreach($field->values as $value){
-					$output .= '<option value="'.$value.'">'.$value.'</option>';
-				}
-				break;
-			case 'text':
-				if ($field->size <= 64) $output .= '<input type="text" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'" maxlength="'.$field->size.'" value="" />';
-				else $output .= '<textarea rows="8" cols="40" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'"></textarea>';
-				break;
-			case 'radio':
-				foreach($field->values as $value){
-					$output .= '<input type="radio" name="'.str_replace(" ","_",$field->name).'" value="'.str_replace(" ","_",$value).'" /> '.$value.'<br />';
-				}
-				break;
-			case 'checkbox':
-				foreach($field->values as $value){
-					$output .= '<input type="checkbox" name="'.str_replace(" ","_",$field->name).'['.$value.']" value="yes" /> '.$value.'<br />';
-				}
-				break;
+		if (in_array($_REQUEST["c"], $field->categories)){
+			$output .= '
+				<tr>
+					<td class="form_label"><label for="'.str_replace(" ","_",$field->name).'">'.$field->name.':</label></td>
+					<td class="form_input">';
+			
+			switch($field->input_type){
+				case 'multiple':
+					$output .= '<input type="text" id="'.str_replace(" ","_",$field->name).'_text" name="'.str_replace(" ","_",$field->name).'_text" maxlength="'.$field->size.'" value="" />';
+					$output .= '<select name="'.str_replace(" ","_",$field->name).'_select" id="'.str_replace(" ","_",$field->name).'_select">';
+					$output .= '<option value="">Select one</option>';
+					foreach($field->values as $value){
+						$output .= '<option value="'.$value.'" onclick="document.getElementById(\''.str_replace(" ","_",$field->name).'_text\').value = \''.$value.'\';">'.$value.'</option>';
+					}
+					$output .= '<input type="text" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'" maxlength="'.$field->size.'" value="" />';
+					break;
+				case 'select':
+					$output .= '<select name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'">
+						<option value="">Select one</option>';
+					foreach($field->values as $value){
+						$output .= '<option value="'.$value.'">'.$value.'</option>';
+					}
+					break;
+				case 'text':
+					if ($field->size <= 64) $output .= '<input type="text" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'" maxlength="'.$field->size.'" value="" />';
+					else $output .= '<textarea rows="8" cols="40" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'"></textarea>';
+					break;
+				case 'radio':
+					foreach($field->values as $value){
+						$output .= '<input type="radio" name="'.str_replace(" ","_",$field->name).'" value="'.str_replace(" ","_",$value).'" /> '.$value.'<br />';
+					}
+					break;
+				case 'checkbox':
+					foreach($field->values as $value){
+						$output .= '<input type="checkbox" name="'.str_replace(" ","_",$field->name).'['.$value.']" value="yes" /> '.$value.'<br />';
+					}
+					break;
+			}
+			
+			$output .= '</td>
+				</tr>';
 		}
-		
-		$output .= '</td>
-			</tr>';
 	}
 	
 	$output .= '		<tr>
@@ -110,16 +127,19 @@ else{
 	if (mysql_num_rows($result) > 0){
 		while($row = mysql_fetch_array($result)){
 			$item = new item($row["id"]);
-			if ($cat_id != $row["item_category"]){
-				if ($cat_id != -1){
-					$output .= '<hr />';
+			
+			if ($item->in_or_below($_REQUEST["c"])){
+				if ($cat_id != $row["item_category"]){
+					if ($cat_id != -1){
+						$output .= '<hr />';
+					}
+					
+					$cat_id = $row["item_category"];
+					$output .= '<p><b>In '.$item->category->get_breadcrumb_links().'</b></p>';
 				}
 				
-				$cat_id = $row["item_category"];
-				$output .= '<p><b>In '.$item->category->get_breadcrumb_links().'</b></p>';
+				$output .= $item->export_teaser();
 			}
-			
-			$output .= $item->export_teaser();
 		}
 	}
 	else{

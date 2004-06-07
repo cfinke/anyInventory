d44931c6c1bddd4fb9bea199ccc42c355af1a366
christopherfink <christopherfink>
2004-06-07 02:30:53 +0000

    Commented the category_processor.php file

--- admin/category_processor.php
+++ admin/category_processor.php
@@ -3,12 +3,15 @@
 include("globals.php");
 
 if ($_REQUEST["action"] == "do_add"){
+	// Add a category.
 	$query = "INSERT INTO `anyInventory_categories` (`name`,`parent`) VALUES ('".$_REQUEST["name"]."','".$_REQUEST["parent"]."')";
 	$result = query($query);
 	
+	// Get the id of the category
 	$this_id = mysql_insert_id();
 	
 	if ($_REQUEST["inherit_fields"] == "yes"){
+		// Add the fields from the parent category
 		$parent = new category($_REQUEST["parent"]);
 		
 		if(is_array($parent->field_ids)){
@@ -19,6 +22,7 @@ if ($_REQUEST["action"] == "do_add"){
 		}
 	}
 	
+	// Add the checked fields
 	if (is_array($_REQUEST["fields"])){
 		foreach($_REQUEST["fields"] as $key => $value){
 			$field = new field($key);
@@ -27,11 +31,14 @@ if ($_REQUEST["action"] == "do_add"){
 	}
 }
 elseif($_REQUEST["action"] == "do_edit"){
+	// Make an object from the unchanged category
 	$old_category = new category($_REQUEST["id"]);
 	
+	// Change the category information
 	$query = "UPDATE `anyInventory_categories` SET `name`='".$_REQUEST["name"]."',`parent`='".$_REQUEST["parent"]."' WHERE `id`='".$_REQUEST["id"]."'";
 	$result = query($query);
 	
+	// Remove the category from all of the fields
 	if (is_array($old_category->field_ids)){
 		foreach($old_category->field_ids as $field_id){
 			$temp_field = new field($field_id);
@@ -39,6 +46,7 @@ elseif($_REQUEST["action"] == "do_edit"){
 		}
 	}
 	
+	// Add the checked fields
 	if (is_array($_REQUEST["fields"])){
 		foreach($_REQUEST["fields"] as $key => $value){
 			$temp_field2 = new field($key);
@@ -47,6 +55,7 @@ elseif($_REQUEST["action"] == "do_edit"){
 	}
 	
 	if ($_REQUEST["apply_fields"] == "yes"){
+		// Apply the fields of this category to all of the children
 		$category = new category($_REQUEST["id"]);
 		
 		$children = get_category_array($category->id);
@@ -62,29 +71,37 @@ elseif($_REQUEST["action"] == "do_edit"){
 	}
 }
 elseif($_REQUEST["action"] == "do_delete"){
+	// Make sure the user clicked "Delete" and not "Cancel"
 	if ($_REQUEST["delete"] == "Delete"){
+		// Create an object from the category
 		$category = new category($_REQUEST["id"]);
 		
+		// Delete the category
 		$query = "DELETE FROM `anyInventory_categories` WHERE `id`='".$_REQUEST["id"]."'"; 
 		$result = query($query);
 		
 		if ($_REQUEST["item_action"] == "delete"){
+			// Delete all of the items in the category
 			$query = "DELETE FROM `anyInventory_items` WEHRE `item_category`='".$category->id."'";
 			$result = query($query);
 		}
 		elseif($_REQUEST["item_action"] == "move"){
+			// Move the items to a different category
 			$query = "UPDATE `anyInventory_items` SET `item_category`='".$_REQUEST["move_items_to"]."'";
 			$result = query($query);
 		}
 		
 		if ($_REQUEST["subcat_action"] == "delete"){
+			// Delete the subcategories
 			delete_subcategories($category);
 		}
 		elseif($_REQUEST["subcat_action"] == "move"){
+			// Move the subcategories
 			$query = "UPDATE `anyInventory_categories` SET `parent`='".$_REQUEST["move_subcats_to"]."' WHERE `parent`='".$category->id."'";
 			$result = query($query);
 		}
 		
+		// Remove all of the fields from this category.
 		remove_from_fields($category->id);
 	}
 }

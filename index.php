4185676960eeafca1cde06bf6e34062736554c6e
christopherfink <christopherfink>
2004-06-10 00:49:02 +0000

    Added alerts to item pages

--- index.php
+++ index.php
@@ -16,53 +16,48 @@ $num_per_column = ceil($category->num_children / 2);
 // Display the breadcrumb links to this category.
 $output .= '<p style="padding-bottom: 5px; padding-top: 0px; padding-left: 0; padding-right: 0; margin-bottom: 10px;border-width: 0px 0px 1px 0px; border-style: solid; border-color: #000000;">'.$category->get_breadcrumb_links().'</p>';
 
+$output .= '<table><tr><td style="width: 85%;">';
+
 if ($_REQUEST["id"]){
 	// A specific item has been requested.
 	$item = new item($_REQUEST["id"]);
 	$output .= $item->export_description();
+	
+	$query = "SELECT `id`,`field_id` FROM `anyInventory_alerts` WHERE `item_ids` LIKE '%\"".$item->id."\"%'";
+	$result = query($query);
+	
+	if (mysql_num_rows($result) > 0){
+		while ($row = mysql_fetch_array($result)){
+			$alert = new alert($row["id"]);
+			$field = new field($row["field_id"]);
+			
+			if (eval('return ("'.addslashes($item->fields[$field->name]).'" '.$alert->condition.' "'.addslashes($alert->value).'");')){
+				if (!$tripped){
+					$output .= '</td><td style="width: 30ex;"><h3>Alerts for this item</h3>';
+					$tripped = true;
+				}
+				
+				$output .= '
+					<table cellspacing="1" cellpadding="2" style="background: #000000; width: 25ex; margin-bottom: 10px;" border="0">
+						<tr style="background: #000000; color: #D3D3A6;">
+							<td>
+								Alert
+							</td>
+						</tr>
+						<tr style="background: #D3D3A6;">
+							<td style="text-align: center;">
+								<b>'.$alert->title.'</b>
+							</td>
+						</tr>
+					</table>';
+			}
+		}
+	}
 }
 else{
 	// We are in a specific category.
 	if ($_REQUEST["c"] == 0){
 		// Display introductory text, check for alerts
-		
-		$query = "SELECT * FROM `anyInventory_alerts` WHERE `time` <= NOW()";
-		$result = query($query);
-		
-		while ($row = mysql_fetch_array($result)){
-			$items = unserialize($row["item_ids"]);
-			
-			foreach ($items as $item_id){
-				$item = new item($item_id);
-				$field = new field($row["field_id"]);
-				
-				$trip_alert = eval('return ($item->fields[$field->name] '.$row["condition"].' $row["value"]);');
-				
-				if ($trip_alert){
-					if (!$tripped){
-						$output .= '<table><tr><td>';
-						$tripped = true;
-					}
-					
-					$output .= '
-						<table cellspacing="1" cellpadding="2" style="background: #000000; width: 25ex; margin-bottom: 10px;" border="0">
-							<tr style="background: #000000; color: #D3D3A6;">
-								<td>
-									Alert
-								</td>
-							</tr>
-							<tr style="background: #D3D3A6;">
-								<td style="text-align: center;">
-									<b>'.$item->name.'</b><br /> '.$row["title"].'<br />
-								</td>
-							</tr>
-						</table>';
-				}
-			}
-		}
-		
-		if ($tripped) $output .= '</td><td>';
-		
 		$output .= '<p style="padding: 15px;">This is the front page and top-level category of anyInventory.  You can <a href="docs/">read the documentation</a> for instructions on using anyInventory, or you can navigate the inventory by clicking on any of the subcategories below; any items in a category will appear below the subcategories.  You can tell where you are in the inventory by the breadcrumb links at the top of each category page.</p>';
 	}
 	
@@ -104,8 +99,42 @@ else{
 		$output .= '</table>';
 	}
 	
-	if ($tripped) $output .= '</td></tr></table>';
+	$query = "SELECT * FROM `anyInventory_alerts` WHERE `time` <= NOW()";
+	$result = query($query);
+	
+	while ($row = mysql_fetch_array($result)){
+		$items = unserialize($row["item_ids"]);
+		$alert = new alert($row["id"]);
+		
+		foreach ($items as $item_id){
+			$item = new item($item_id);
+			$field = new field($row["field_id"]);
+			
+			if (eval('return ("'.addslashes($item->fields[$field->name]).'" '.$alert->condition.' "'.addslashes($alert->value).'");')){
+				if (!$tripped){
+					$output .= '</td><td>';
+					$tripped = true;
+				}
+				
+				$output .= '
+					<table cellspacing="1" cellpadding="2" style="background: #000000; width: 25ex; margin-bottom: 10px;" border="0">
+						<tr style="background: #000000; color: #D3D3A6;">
+							<td>
+								Alert
+							</td>
+						</tr>
+						<tr style="background: #D3D3A6;">
+							<td style="text-align: center;">
+								<b>'.$row["title"].'</b><br /><a href="'.$_SERVER["PHP_SELF"].'?id='.$item->id.'">'.$item->name.'</a>
+							</td>
+						</tr>
+					</table>';
+			}
+		}
+	}
 }
+
+$output .= '</td></tr></table>';
 
 display($output);
 

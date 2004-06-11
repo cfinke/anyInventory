fe2dd774f91b47314e1c243fef85e57b9c5d38de
christopherfink <christopherfink>
2004-06-11 00:04:04 +0000

    Rewrote alerts documentation

--- docs/alerts.php
+++ docs/alerts.php
@@ -6,9 +6,9 @@ $title = "anyInventory: Help > Alerts and Adding Alerts";
 
 $output .= '
 	<h2>Alerts</h2>
-	<p>Alerts allow you to have anyInventory notify you in certain situations, all of which you define.  For example, let\'s say that you have inventoried your household items, and you want to be notified when the number of rolls of toilet paper is one or less (for the obvious reason).  The way to do this in anyInventory is to add an alert to the "Toilet Paper" item, which you already would have added in the category "Bathroom Items".</p>
+	<p>Alerts allow you to have anyInventory notify you when certain conditions arise. For example, say that you are using anyInventory to keep track of the office supplies at your workplace, and you need to know when there is only one printer cartridge left.  anyInventory alerts allow you to do just that.</p>
 	<h2><a name="adding">Adding Alerts</a></h2>
-	<p>To <a href="../admin/add_alert.php">add an alert</a>, you must first choose the category to which the item or items belong.  Note that only the categories which contain one or more items will be shown in this list.</p>
+	<p>To <a href="../admin/add_alert.php">add an alert</a>, you must first choose the category to which the item or items belong.  Note that only the categories which contain one or more items will be shown in the list.</p>
 	<p>Once you have chosen a category, you can begin specifying the conditions of the alert.  Here is a what an alert addition page might look like:</p>
 	<form>
 		<table>
@@ -20,9 +20,9 @@ $output .= '
 				<td class="form_label"><label for="c">Applies to:</label></td>
 				<td class="form_input">
 					<select name="i[]" id="i[]" multiple="multiple">
-						<option value="">Shampoo</option>
-						<option value="">Soap</option>
-						<option value="">Toilet Paper</option>
+						<option value="">Printer Cartridges</option>
+						<option value="">Paper</option>
+						<option value="">Toner</option>
 					</select>
 				</td>
 			</tr>
@@ -76,21 +76,21 @@ $output .= '
 			</tr>
 		</table>
 	</form>
-	<p>Begin by specifying a title.  This is what you will see when the alert is activated. In this situation, an appropriate title would be "Low on Toilet Paper."</p>
-	<p>After that, select the items that you want to apply the alert to. (You can choose multiple items by holding down the Ctrl key.)</p>
+	<p>Begin by specifying a title.  This is what you will see when the alert is activated. In this situation, an appropriate title would be "Low Quantity."</p>
+	<p>After that, select the items that you want to apply the alert to. (You can choose multiple items by holding down the Ctrl key, so if you wanted to know when there is one printer cartridge or less or one toner cartridge or less, you could select both items.)</p>
 	<form>
 		<table>
 			<tr>
 				<td class="form_label"><label for="name">Alert Title:</label></td>
-				<td class="form_input"><input type="text" name="title" id="title" value="Low on Toilet Paper" maxlength="255" />
+				<td class="form_input"><input type="text" name="title" id="title" value="Low Quantity" maxlength="255" />
 			</tr>
 			<tr>
 				<td class="form_label"><label for="c">Applies to:</label></td>
 				<td class="form_input">
 					<select name="i[]" id="i[]" multiple="multiple">
-						<option value="">Shampoo</option>
-						<option value="">Soap</option>
-						<option value="" selected="selected">Toilet Paper</option>
+						<option value="" selected="selected">Printer Cartridges</option>
+						<option value="">Paper</option>
+						<option value="">Toner</option>
 					</select>
 				</td>
 			</tr>
@@ -140,7 +140,7 @@ $output .= '
 					</select>
 					<select name="day" id="day"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9" selected="selected">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option>			</select>,
 					<select name="year" id="year"><option value="2004">2004</option><option value="2005">2005</option><option value="2006">2006</option><option value="2007">2007</option><option value="2008">2008</option><option value="2009">2009</option><option value="2010">2010</option><option value="2011">2011</option><option value="2012">2012</option><option value="2013">2013</option><option value="2014">2014</option><option value="2015">2015</option><option value="2016">2016</option><option value="2017">2017</option><option value="2018">2018</option><option value="2019">2019</option><option value="2020">2020</option><option value="2021">2021</option><option value="2022">2022</option><option value="2023">2023</option>			</select>
-				</td>
+					</td>
 			</tr>
 		</table>
 	</form>
@@ -149,15 +149,15 @@ $output .= '
 		<table>
 			<tr>
 				<td class="form_label"><label for="name">Alert Title:</label></td>
-				<td class="form_input"><input type="text" name="title" id="title" value="Low on Toilet Paper" maxlength="255" />
+				<td class="form_input"><input type="text" name="title" id="title" value="Low Quantity" maxlength="255" />
 			</tr>
 			<tr>
 				<td class="form_label"><label for="c">Applies to:</label></td>
 				<td class="form_input">
 					<select name="i[]" id="i[]" multiple="multiple">
-						<option value="">Shampoo</option>
-						<option value="">Soap</option>
-						<option value="" selected="selected">Toilet Paper</option>
+						<option value="" selected="selected">Printer Cartridges</option>
+						<option value="">Paper</option>
+						<option value="">Toner</option>
 					</select>
 				</td>
 			</tr>
@@ -207,7 +207,7 @@ $output .= '
 					</select>
 					<select name="day" id="day"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9" selected="selected">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option>			</select>,
 					<select name="year" id="year"><option value="2004">2004</option><option value="2005">2005</option><option value="2006">2006</option><option value="2007">2007</option><option value="2008">2008</option><option value="2009">2009</option><option value="2010">2010</option><option value="2011">2011</option><option value="2012">2012</option><option value="2013">2013</option><option value="2014">2014</option><option value="2015">2015</option><option value="2016">2016</option><option value="2017">2017</option><option value="2018">2018</option><option value="2019">2019</option><option value="2020">2020</option><option value="2021">2021</option><option value="2022">2022</option><option value="2023">2023</option>			</select>
-				</td>
+					</td>
 			</tr>
 		</table>
 	</form>
@@ -221,7 +221,7 @@ $output .= '
 		</tr>
 		<tr style="background: rgb(211, 211, 166);">
 			<td style="text-align: center;">
-				<b>Low on Toilet Paper</b><br><a href="">Toilet Paper</a>
+				<b>Low Quantity</b><br><a href="">Printer Cartridges</a>
 			</td>
 		</tr>
 	</table>

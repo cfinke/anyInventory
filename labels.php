fb207326bb4897f63a96d49c021fde2fa1d5f13b
christopherfink <christopherfink>
2004-06-16 21:05:47 +0000

    Added help links and cleaned up pages

--- labels.php
+++ labels.php
@@ -2,7 +2,21 @@
 
 require("globals.php");
 
-if ($_REQUEST["action"] == "generate"){
+$title = "anyInventory: Labels";
+
+if (!function_exists('imagecreate') ||
+    !function_exists('imagecolorallocate') ||
+	!function_exists('imagettftext') ||
+	!function_exists('imagestring') ||
+	!function_exists('imagecopyresized') ||
+	!function_exists('imagedestroy') ||
+	!function_exists('imagepng')){
+	
+	$output .= '
+		<h2>Generate Labels</h2>
+		<p>You do not have all of the PHP functions needed to create labels installed.  These functions are <a href="http://us3.php.net/manual/en/function.imagecreate.php">imagecreate</a>, <a href="http://us3.php.net/manual/en/function.imagecolorallocate.php">imagecolorallocate</a>, <a href="http://us3.php.net/manual/en/function.imagettftext.php">imagettftext</a>, <a href="http://us3.php.net/manual/en/function.imagestring.php">imagestring</a>, <a href="http://us3.php.net/manual/en/function.imagecopyresized.php">imagecopyresized</a>, <a href="http://us3.php.net/manual/en/function.imagedestroy.php">imagedestroy</a>, and <a href="http://us3.php.net/manual/en/function.imagepng.php">imagepng</a>.  One or more of these functions is not installed.</p>';
+}
+elseif ($_REQUEST["action"] == "generate"){
 	if (!is_array($_REQUEST["i"])) $_REQUEST["i"] = array($_REQUEST["i"]);
 	
 	foreach($_REQUEST["i"] as $item_id){
@@ -12,12 +26,24 @@ if ($_REQUEST["action"] == "generate"){
 	}
 }
 elseif (!isset($_REQUEST["c"])){
-	$output .= '<h2>Generate Labels</h2>
+	$output .= '
+		<table style="width: 100%;"><tr><td><h2>Generate Labels</h2></td><td style="text-align: right;"><a href="docs/labels.php">Help with labels</a></td></tr></table>
+		<p>Select the categories top which the items belong for which you want to produce labels.  All categories that you select must have at least one field in common.</p>
 		<form action="labels.php" method="post">
-			<select name="c[]" id="c[]" multiple="multiple">
-				'.get_category_options().'
-			</select>
-			<input type="submit" name="submit" value="Submit" />
+			<table>
+				<tr>
+					<td class="form_label">Categories:</td>
+					<td class="form_input">
+						<select name="c[]" id="c[]" multiple="multiple">
+							'.get_category_options().'
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td style="text-align: center;"><input type="submit" name="submit" value="Submit" /></td>
+				</tr>
+			</table>
 		</form>';
 }
 elseif (!isset($_REQUEST["i"])){
@@ -40,7 +66,8 @@ elseif (!isset($_REQUEST["i"])){
 	}
 	else{
 		$output .= '
-			<h2>Generate Labels</h2>
+			<table style="width: 100%;"><tr><td><h2>Generate Labels</h2></td><td style="text-align: right;"><a href="docs/labels.php">Help with labels</a></td></tr></table>
+			<p>Select the field from which you want to produce the barcode and the items for which you want to produce a label.</p>
 			<form action="labels.php" method="post">
 				<input type="hidden" name="action" value="generate" />
 				<table>
@@ -76,7 +103,7 @@ elseif (!isset($_REQUEST["i"])){
 					</tr>
 					<tr>
 						<td>&nbsp;</td>
-						<td><input type="submit" name="submit" value="Submit" /></td>
+						<td style="text-align: center;"><input type="submit" name="submit" value="Submit" /></td>
 					</tr>
 				</table>
 			</form>';

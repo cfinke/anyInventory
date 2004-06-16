929c3211cc87cba071bb07ec9f46a408118b77c4
christopherfink <christopherfink>
2004-06-16 17:07:22 +0000

    Added error for labels

--- error_handler.php
+++ error_handler.php
@@ -7,6 +7,7 @@ $errors = array();
 $errors[0] = "There is already a field with the name you specified.  If you wish to add a field to multiple categories, you can do so by editing the field and selecting several categories by holding down the Ctrl key.";
 $errors[1] = "The default value for a select or radio field must be included in the list of values.";
 $errors[2] = "There must be items in a category for you to add an alert in it.";
+$errors[3] = "There were no common fields in the categories you selected.";
 
 $output = '<h2>Error</h2>'.$errors[$_REQUEST["eid"]];
 

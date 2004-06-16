5aaa21260eba689312183324accba95d2ad117b8
christopherfink <christopherfink>
2004-06-16 17:17:11 +0000

    Added labels to menu

--- header.php
+++ header.php
@@ -26,10 +26,11 @@
 						<b>Main Menu: </b>
 						[ <a href="<?php echo $DIR_PREFIX; ?>index.php">home</a> ]
 						[ <a href="<?php echo $DIR_PREFIX; ?>search.php">advanced search</a> ]
+						[ <a href="<?php echo $DIR_PREFIX; ?>labels.php">labels</a> ]
 						[ <a href="<?php echo $DIR_PREFIX; ?>admin/fields.php">fields</a> ]
 						[ <a href="<?php echo $DIR_PREFIX; ?>admin/categories.php">categories</a> ]
 						[ <a href="<?php echo $DIR_PREFIX; ?>admin/items.php">items</a> ]
-						[ <a href="<?php echo $DIR_PREFIX; ?>admin/alerts.php">alerts</a> ]						
+						[ <a href="<?php echo $DIR_PREFIX; ?>admin/alerts.php">alerts</a> ]
 						[ <a href="<?php echo $DIR_PREFIX; ?>docs/">help</a> ]
 					</div>	
 				</td>

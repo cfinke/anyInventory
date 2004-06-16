9a67c25c5d121e5368308948322a893f3ecfddad
christopherfink <christopherfink>
2004-06-16 21:05:13 +0000

    Added check to make sure necessary functions are installed.

--- docs/labels.php
+++ docs/labels.php
@@ -6,6 +6,28 @@ $title = "anyInventory: Help > Labels";
 
 $output .= '
 	<h2>Labels</h2>
+	<p>anyInventory, as of version 1.5, allows you to create a barcode label from any field of an item.  For example, let\'s say that you had bought the DVD of the movie "Animal House," and you added it to your inventory, tracking the UPC, the ISBN, and the name.  You could produce a label from the UPC in one of two ways:</p>
+	<ol>
+		<li>On the item page for "Animal House," each field name is a link to a barcode label of that field\'s value.  To get the UPC label, you would just click on UPC.</li>
+		<li>You could go to the <a href="../labels.php">labels page</a> and follow the directions to produce a label for the UPC field of "Animal House."  This method also allows you to make labels for multiple items.</li>
+	</ol>
+	<p>After you choose one of these two methods, you will be presented with a graphic that looks like this:</p>
+	<p style="text-align: center;"><img src="images/sample_label.png" alt="Animal House UPC label" /></p>
+	<p>A future version of anyInventory will allow you to create PDFs of labels that you can print on standard-size label sheets.</p>
+	<p><i>Note: This feature only works if the necessary functions are installed.  <b>According to a self-test, the functions necessary for label production to work are ';
+
+if (!function_exists('imagecreate') ||
+    !function_exists('imagecolorallocate') ||
+	!function_exists('imagettftext') ||
+	!function_exists('imagestring') ||
+	!function_exists('imagecopyresized') ||
+	!function_exists('imagedestroy') ||
+	!function_exists('imagepng')){
+	
+	$output .= ' not ';
+}
+
+$output .= ' installed.</b></i></p>
 	<div style="float: left;"><a href="deleting_alerts.php">&lt;&lt; Previous: Deleting Alerts</a></div>
 	<div style="text-align: right;"><a href="searching.php">Next: Searching &gt;&gt;</a></div>';
 
--- label_processor.php
+++ label_processor.php
@@ -2,33 +2,55 @@
 
 include("globals.php");
 
+if (!function_exists('imagecreate') ||
+    !function_exists('imagecolorallocate') ||
+	!function_exists('imagettftext') ||
+	!function_exists('imagestring') ||
+	!function_exists('imagecopyresized') ||
+	!function_exists('imagedestroy') ||
+	!function_exists('imagepng')){
+	
+	$output .= '<p>You do not have all of the PHP functions needed to create labels installed.  These functions are <a href="http://us3.php.net/manual/en/function.imagecreate.php">imagecreate</a>, <a href="http://us3.php.net/manual/en/function.imagecolorallocate.php">imagecolorallocate</a>, <a href="http://us3.php.net/manual/en/function.imagettftext.php">imagettftext</a>, <a href="http://us3.php.net/manual/en/function.imagestring.php">imagestring</a>, <a href="http://us3.php.net/manual/en/function.imagecopyresized.php">imagecopyresized</a>, <a href="http://us3.php.net/manual/en/function.imagedestroy.php">imagedestroy</a>, and <a href="http://us3.php.net/manual/en/function.imagepng.php">imagepng</a>.  One or more of these functions is not installed.</p>';
+	
+	display($output);
+}
+
+// Create the item object
 $item = new item($_REQUEST["i"]);
 
-$im = imagecreate(400, 400);
+// Create the image.
+$im = imagecreate(600, 70);
+
+// Color the background white
 $white = imagecolorallocate($im, 255, 255, 255);
+
+// Set the color for the text.
 $black = imagecolorallocate($im, 0, 0, 0);
 
-// Replace path by your own font path
-$boundary = imagettftext($im, 12, 0, 0, 50, $black, realpath("fonts/IDAutomationHC39M.ttf"),"!".$item->fields[$_REQUEST["f"]]."!");
+// Write the barcode.
+$boundaries = imagettftext($im, 12, 0, 0, 50, $black, realpath("fonts/IDAutomationHC39M.ttf"),"!".$item->fields[$_REQUEST["f"]]."!");
 
-$width = $boundary[2];
-$height = $boundary[1];
-$offset = 37;
-
+// This is the width of one character in pixels.
 $char_width = 5;
 
-$offset = ($width - (strlen($item->name) * $char_width)) / 2;
+// Figure the offset for centering the text
+$offset = ($boundaries[2] - (strlen($item->name) * $char_width)) / 2;
 
+// Write the item name to the label.
 imagestring($im, 1, $offset, 0, $item->name, $black);
 
-$new_image = imagecreate($width, $height);
-imagecopyresized($new_image, $im, 0, 0, 0, 0, $width, $height, $width, $height);
+// Crop the image
+$new_image = imagecreate($boundaries[2], $boundaries[1]);
+imagecopyresized($new_image, $im, 0, 0, 0, 0, $boundaries[2], $boundaries[1], $boundaries[2], $boundaries[1]);
 
-imagedestroy($im);	
+// Delete the old image.
+imagedestroy($im);
 
+// Send the new image to the browser
 header("Content-type: image/png");
 imagepng($new_image);
 
+// Delete the new image.
 imagedestroy($new_image);
 
 exit;

a0f2a4375c45af515969e588f87433a7540d3761
christopherfink <christopherfink>
2004-06-18 00:31:26 +0000

    Updated What's Next documentation to match TODO

--- docs/whats_next.php
+++ docs/whats_next.php
@@ -7,7 +7,8 @@ $title = "anyInventory: Help > What's Next?";
 $output .= '
 	<h2>What\'s Next?</h2>
 	<p>This page details some of the features that will be added to anyInventory in future releases.</p>
-	<p><b>Timed alerts:</b> This feature will allow you to create alerts that are strictly time-based, rather than being based on additional conditions of an item.</p>
+	<p><b>More search options:</b> Soon, anyInventory will have a better advanced search page.  You will be able to specify, for each field, if the value you enter should match items with values equal to, not equal to, less than, or greater than that value.  Other improvements are planned as well.</p>
+	<p><b>Built-in password protection for the admin section:</b> Version 1.7 will have a built-in ability to password protect the administrative side of anyInventory to take that responsibility away from the individual user.</p>
 	<p><b>Label sheets:</b> This feature will allow you to create standard-size sheets of labels for any items in your inventory.</p>
 	<p>If you have any suggestions, comments, or complaints, contact <a href="mailto:chris@efinke.com">chris@efinke.com</a>.</p>
 	<div style="float: left;"><a href="searching.php">&lt;&lt; Previous: Searching</a></div>';

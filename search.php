1c341c75fd8d30d7d4628c2befc6478d524a8db4
christopherfink <christopherfink>
2004-07-01 03:46:09 +0000

    Removed advanced search and brought back quick search

--- search.php
+++ search.php
@@ -2,183 +2,43 @@
 
 include("globals.php");
 
-$title = "anyInventory: Search";
+$title = "anyInventory: Search Results";
 
-// The default category is the Top Level.
-if (!isset($_REQUEST["c"])) $_REQUEST["c"] = array(0);
-
-if (!$_REQUEST["action"]){
-	$breadcrumbs = "Search";
+if ($_REQUEST["action"] == "quick_search"){
+	$breadcrumbs = "Search Results: ".$_REQUEST["q"];
 	
-	$output .= '
-		<table class="standardTable" cellspacing="0">
-			<tr class="tableHeader">
-				<td>Search</td>
-				<td style="text-align: right;">[<a href="docs/searching.php">Help</a>]</a></td>
-			</tr>
-			<tr>
-				<td class="tableData" colspan="2">
-					<table>
-						<form action="'.$_SERVER["PHP_SELF"].'" method="get">
-							<tr>
-								<td class="form_label">Limit search to:</td>
-								<td>
-									<select name="c[]" id="c[]" multiple="multiple" size="10" style="width: 100%;">
-										'.get_category_options($_REQUEST["c"]).'
-									</select>
-								</td>
-							</tr>
-							<tr>
-								<td class="submitButtonRow" colspan="2"><input type="submit" name="submit" value="Update Categories" /></td>
-							</tr>
-						</form>
-						</tr>
-						<form action="'.$_SERVER["PHP_SELF"].'" method="get">
-							<input type="hidden" name="action" value="search" />
-							<input type="hidden" name="c" value="'.htmlentities(serialize($_REQUEST["c"])).'" />
-							<tr>
-								<td class="form_label"><label for="name">Name:</label></td>
-								<td class="form_input"><input type="text" name="name" id="name" value="'.$item->name.'" maxlength="64" style="width: 100%;" />
-							</tr>';
+	$search_terms = explode(" ",$_REQUEST["q"]);
+	$search_fields = array("name");
 	
-	// Get each field and output a search field for it.
-	$query = "SELECT `id` FROM `anyInventory_fields` ORDER BY `name`";
-	$result = mysql_query($query) or die(mysql_error() . '<br /><br />'. $query);
+	$output .= '<table>';
 	
-	while($row = mysql_fetch_array($result)){
-		$field = new field($row["id"]);
+	if (trim($_REQUEST["q"]) != ''){
+		$query = "SELECT `name` FROM `anyInventory_fields`";
+		$result = mysql_query($query) or die(mysql_error() . '<br /><br />' . $query);
 		
-		if ($field->input_type != 'file'){
-			if (is_array($_REQUEST["c"])){
-				foreach($_REQUEST["c"] as $cat){
-					if (in_array($cat, $field->categories)){
-						$output .= '
-							<tr>
-								<td class="form_label"><label for="'.str_replace(" ","_",$field->name).'">'.$field->name.':</label></td>
-								<td class="form_input">';
-						
-						switch($field->input_type){
-							case 'multiple':
-								$output .= '<input type="text" id="'.str_replace(" ","_",$field->name).'_text" name="'.str_replace(" ","_",$field->name).'_text" maxlength="'.$field->size.'" value="" />';
-								$output .= '<select name="'.str_replace(" ","_",$field->name).'_select" id="'.str_replace(" ","_",$field->name).'_select">';
-								$output .= '<option value="">Select one</option>';
-								
-								if (is_array($field->values)){
-									foreach($field->values as $value){
-										$output .= '<option value="'.$value.'" onclick="document.getElementById(\''.str_replace(" ","_",$field->name).'_text\').value = \''.$value.'\';">'.$value.'</option>';
-									}
-								}
-								
-								$output .= '<input type="text" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'" maxlength="'.$field->size.'" value="" />';
-								break;
-							case 'select':
-								$output .= '<select name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'">
-									<option value="">Select one</option>';
-								
-								if (is_array($field->values)){
-									foreach($field->values as $value){
-										$output .= '<option value="'.$value.'">'.$value.'</option>';
-									}
-								}
-								
-								break;
-							case 'text':
-								$output .= '<input type="text" name="'.str_replace(" ","_",$field->name).'" id="'.str_replace(" ","_",$field->name).'" maxlength="'.$field->size.'" value="" style="width: 100%;" />';
-								break;
-							case 'radio':
-								if (is_array($field->values)){
-									foreach($field->values as $value){
-										$output .= '<input type="radio" name="'.str_replace(" ","_",$field->name).'" value="'.str_replace(" ","_",$value).'" /> '.$value.'<br />';
-									}
-								}
-								
-								break;
-							case 'checkbox':
-								if (is_array($field->values)){
-									foreach($field->values as $value){
-										$output .= '<input type="checkbox" name="'.str_replace(" ","_",$field->name).'['.$value.']" value="yes" /> '.$value.'<br />';
-									}
-								}
-								
-								break;
-						}
-						
-						$output .= '</td>
-							</tr>';
-						
-						break;
-					}
-				}
-			}
-		}
-	}
-	
-	$output .= '		
-						<tr>
-							<td class="submitButtonRow" colspan="2"><input type="submit" name="submit" id="submit" value="Search" /></td>
-						</tr>
-					</form>
-				</td>
-			</tr>
-		</table>';
-}
-else{
-	$breadcrumbs = "Search Results";
-	
-	$query = "SELECT `name` FROM `anyInventory_fields`";
-	$result = mysql_query($query) or die(mysql_error() . '<br /><br />'. $query);
-	
-	while ($row = mysql_fetch_array($result)){
-		$fields[] = $row["name"];
-	}
-	
-	$query = "SELECT `id`,`item_category` FROM `anyInventory_items` WHERE 1 ";
-	
-	if ($_REQUEST["name"] != ''){
-		$name_parts = explode(" ",$_REQUEST["name"]);
-		$query .= " AND (";
-		
-		if (is_array($name_parts)){
-			foreach($name_parts as $name_part){
-				$query .= " (`name` LIKE '%".$name_part."%') AND ";
-			}
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)){
+			$search_fields[] = $row["name"];
 		}
 		
-		$query = substr($query, 0, strlen($query) - 5) . ") ";
-	}
-	
-	if (is_array($_REQUEST)){
-		foreach($_REQUEST as $key => $value){
-			if ((trim($value) != '') && in_array(str_replace("_"," ",$key), $fields)){
-				if (is_array($value)){
-					foreach($value as $key1 => $val){
-						$query .= " AND `".str_replace("_"," ",$key)."` LIKE '%".$key1."%' ";
-					}
-				}
-				else{
-					$query .= " AND `".str_replace("_"," ",$key)."` LIKE '%".$value."%' ";
-				}
+		$search_query = "SELECT `id`,`item_category` FROM `anyInventory_items` WHERE 1 AND ( ";
+		
+		foreach($search_terms as $search_term){
+			foreach($search_fields as $search_field){
+				$search_query .= " `".$search_field."` LIKE '%".$search_term."%' OR ";
 			}
-			elseif((trim($value) != '') && (in_array(str_replace("_"," ",str_replace("_text","",$key)), $fields))){
-				$query .= " AND `".str_replace("_"," ",str_replace("_text","",$key))."` LIKE '%".$value."%' ";
-			}
-		}
-	}
-	
-	$query .= " ORDER BY `item_category`,`Name`";
-	$result = mysql_query($query) or die(mysql_error() . '<br /><br />'. $query);
-	
-	$cat_id = -1;
-	
-	$_REQUEST["c"] = unserialize(stripslashes($_REQUEST["c"]));
-	
-	$output .= '<table class="standardTable" cellspacing="0">';
-	
-	if (mysql_num_rows($result) > 0){
-		while($row = mysql_fetch_array($result)){
-			$item = new item($row["id"]);
 			
-			if (in_array($row["item_category"], $_REQUEST["c"])){
+			$search_query = substr($search_query,0,strlen($search_query) - 3)." ) AND ( ";
+		}
+		
+		$search_query = substr($search_query,0,strlen($search_query) - 6)." ORDER BY `item_category`";
+		$search_result = mysql_query($search_query) or die(mysql_error() . '<br /><br />' . $search_query);
+		
+		$cat_id = -1;
+		
+		if (mysql_num_rows($search_result) > 0){
+			while($row = mysql_fetch_array($search_result)){
+				$item = new item($row["id"]);
+				
 				if ($cat_id != $row["item_category"]){
 					$cat_id = $row["item_category"];
 					$output .= '
@@ -188,6 +48,9 @@ else{
 				
 				$output .= '<tr><td>'.$item->export_teaser().'</td></tr>';
 			}
+		}
+		else{
+			$output .= '<tr class="tableHeader"><td>No matching results</td></tr><tr><td class="tableData">There were no items that matched your search conditions.</td></tr>';
 		}
 	}
 	else{

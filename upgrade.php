e43cd1e746927ee03c9ee9b0458581b8555c75e7
christopherfink <christopherfink>
2004-07-15 00:47:50 +0000

    Fixed some conflicts in upgrade

--- upgrade.php
+++ upgrade.php
@@ -355,6 +355,54 @@ if ($_POST["action"] == "upgrade"){
 				
 				$query = "ALTER TABLE `anyInventory_alerts` ADD `expire_time` TIMESTAMP NOT NULL AFTER `time`";
 				@$db->query($query);
+				
+				// The following code uses mysql-only commands because I'm not sure of all of the DB equivalents.
+				// It's a moot point because users of pre-1.9 versions could only be using MySQL anyway
+				
+				$query = "CREATE TABLE `anyInventory_values` (
+					`item_id` int( 11 ) NOT NULL default '0',
+					`field_id` int( 11 ) NOT NULL default '0',
+					`value` text NOT NULL ,
+					UNIQUE KEY `item_id` ( `item_id` , `field_id` )
+					) TYPE = MYISAM";
+				@mysql_query($query);
+				
+				$query = "SELECT * FROM `anyInventory_categories`";
+				$result = mysql_query($query) or die(mysql_error() . '<br />' . $query);
+				
+				while ($row = mysql_fetch_array($result)){
+					$newquery = "SELECT * FROM `anyInventory_fields` WHERE `categories` LIKE '%\"".$row["id"]."\"%'";
+					$newresult = mysql_query($newquery) or die(mysql_error() . '<br />' . $newquery);
+					
+					$newestquery = "SELECT * FROM `anyInventory_items` WHERE `item_category`='".$row["id"]."' ORDER BY `id`";
+					$newestresult = mysql_query($newestquery) or die(mysql_error() . '<br />' . $newestquery);
+					
+					while ($newestrow = mysql_fetch_array($newestresult)){
+						while ($newrow = mysql_fetch_array($newresult)){
+							$insert_query = "INSERT INTO `anyInventory_values` 
+										(`item_id`,`field_id`,`value`)
+										VALUES
+										('".$newestrow["id"]."',
+										 '".$newrow["id"]."',
+										 '".$newestrow[$newrow["name"]]."')";
+							@mysql_query($insert_query);
+						}
+						
+						mysql_data_seek($newresult, 0);
+					}
+				}
+				
+				$query = "SHOW COLUMNS FROM `anyInventory_items`";
+				$result = mysql_query($query) or die(mysql_error() . '<br />' . $query);
+				
+				while ($row = mysql_fetch_array($result)){
+					if (($row["Field"] != 'id') && ($row["Field"] != 'name') && ($row["Field"] != 'item_category')){
+						$newquery = "ALTER TABLE `anyInventory_items` DROP `".$row["Field"]."`";
+						mysql_query($newquery) or die(mysql_error() . '<br />' . $newquery);
+					}
+				}
+				
+				/////////////////////////////////
 		}
 		
 		// Attempt to write the globals file.
@@ -518,14 +566,16 @@ elseif(!$globals_error){
 							<td class="form_input"><input type="text" name="db_name" id="db_name" value="'.stripslashes($_POST["db_name"]).'" /></td>
 						</tr>
 						<tr>
-                                                        <td class="form_label"><label for="db_type">DB Type:</label></td>
-							<td class="form_input"><select name="db_type">
-								<option selected>mysql</option>
-								<option>pgsql</option>
-								<option>oci8</option>
-							</select></td>
+							<td class="form_label"><label for="db_type">DB Type:</label></td>
+							<td class="form_input">
+								<select name="db_type">
+									<option value="mysql"';if($_REQUEST["db_type"] == 'mysql') $output .= ' selected="selected"'; $output .= '>MySQL</option>
+									<option value="pgsql"';if($_REQUEST["db_type"] == 'pgsql') $output .= ' selected="selected"'; $output .= '>PosgreSQL</option>
+									<option value="oci8"';if($_REQUEST["db_type"] == 'oci8') $output .= ' selected="selected"'; $output .= '>Oracle</option>
+								</select>
+							</td>
 						</tr>
-						<tr>						
+						<tr>
 							<td class="form_label"><input onclick="toggle();" type="checkbox" name="password_protect_view" id="password_protect_view" value="yes"'.$pp_view_checked.' /></td>
 							<td class="form_input"><label for="password_protect">Password protect entire inventory</label></td>
 						</tr>

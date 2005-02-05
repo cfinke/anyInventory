9151e5d882575a86192e23b9215e14651b719e99
christopherfink <christopherfink>
2005-02-05 07:32:23 +0000

    *** empty log message ***

---
+++ get_file.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("globals.php");
+
+if (isset($_REQUEST["fid"])){
+	$query = "SELECT * FROM " . $db->quoteIdentifier("anyInventory_file_data") . " WHERE " . $db->quoteIdentifier("file_id") . "='".$_REQUEST["fid"]."' ORDER BY " . $db->quoteIdentifier("part_id") . " ASC";
+	$result = $db->query($query);
+	
+	$file_string = '';
+	
+	while ($row = $result->fetchRow()){
+		$file_string .= $row["data"];
+	}
+	
+	$file_string = base64_decode($file_string);
+	
+	$query = "SELECT * FROM " . $db->quoteIdentifier("anyInventory_files") . " WHERE " . $db->quoteIdentifier("id") . "='".$_REQUEST["fid"]."'";
+	$result = $db->query($query);
+	$row = $result->fetchRow();
+	
+	header("Content-Type: " . $row["file_type"]);
+	header("Content-Length: " . strlen($file_string));
+	header("Content-Disposition: attachment; filename=\"".$row["file_name"]."\"");
+	echo $file_string;
+}
+
+?>
